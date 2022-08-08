8a616a730e21c08f1b8b0efaaeaac6b8770b2bab
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-08 15:05:20 +0100

    Added th header component

--- src/components/TodoContainer.jsx
+++ src/components/TodoContainer.jsx
@@ -1,31 +1,35 @@
-import React, { Component } from 'react'
-import TodoList from './TodoList';
+import React, { Component } from "react";
+import Header from "./Header";
+import TodoList from "./TodoList";
 
 export class TodoContainer extends Component {
-    state = {
-        todos: [
-          {
-            id: 1,
-            title: "Setup development environment",
-            completed: true
-          },
-          {
-            id: 2,
-            title: "Develop website and add content",
-            completed: false
-          },
-          {
-            id: 3,
-            title: "Deploy to live server",
-            completed: false
-          }
-        ]
-       };
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: "Setup development environment",
+        completed: true,
+      },
+      {
+        id: 2,
+        title: "Develop website and add content",
+        completed: false,
+      },
+      {
+        id: 3,
+        title: "Deploy to live server",
+        completed: false,
+      },
+    ],
+  };
   render() {
     return (
-     <TodoList  todos={this.state.todos}/>
-    )
+      <>
+        <Header />
+        <TodoList todos={this.state.todos} />
+      </>
+    );
   }
 }
 
-export default TodoContainer
+export default TodoContainer;
