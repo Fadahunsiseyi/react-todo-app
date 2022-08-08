e323eda33cc12d4433cfbb4e8e616277d292cd69
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-08 11:19:20 +0100

    Added the todo container component

---
+++ src/components/TodoContainer.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class TodoContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello from Create React App</h1>
+        <p>I am in a React Component!</p>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer
