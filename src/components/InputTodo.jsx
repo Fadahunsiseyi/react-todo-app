212a6a581ee80beda2bcef5558729cfae0e0b99c
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-09 10:04:41 +0100

    Added the inputtodo component

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class InputTodo extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default InputTodo
