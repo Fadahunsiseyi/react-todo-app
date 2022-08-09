d6422449d6c1bfb867f92bc426701f1438fb781e
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-09 19:41:26 +0100

    Converted the input todo component to a functional component

--- src/functionBased/components/InputTodo.jsx
+++ src/functionBased/components/InputTodo.jsx
@@ -1,44 +1,33 @@
-import React, { Component } from "react";
+import React,{useState} from 'react'
 
-export class InputTodo extends Component {
-    state = {
-        title: '',
-      }
-      onChange=(e)=> {
-        console.log('I am changing')
-        this.setState({
-            [e.target.name]: e.target.value
-        })
-      }
-      handleSubmit=(e)=> {
-        e.preventDefault()
-      if(this.state.title.trim()) {
-        this.props.addTodoProps(this.state.title)
-        this.setState({
-            title: ''
-        })
-      }
-      else {
-        alert("Please write item")
-    }
-      }
-  render() {
-    return (
-      <div>
-        <form onSubmit={this.handleSubmit} className="form-container">
-          <input
-           type="text"
-            placeholder="Add Todo..."
-             value={this.state.title}
-             onChange={this.onChange}
-             name='title'
-             className="input-text"
-            />
-          <button className="input-submit">Submit</button>
-        </form>
-      </div>
-    );
+const InputTodo = (props) => {
+  const [title, setTitle] = useState('')
+  const onChange = e => {
+    setTitle(e.target.value)
   }
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    if (title.trim()) {
+      props.addTodoProps(title)
+      setTitle("")
+    } else {
+      alert("Please write item")
+    }
+  }
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add todo..."
+        value={title}
+        name="title"
+        onChange={onChange}
+      />
+      <button className="input-submit">Submit</button>
+    </form>
+  )
 }
 
-export default InputTodo;
+export default InputTodo
