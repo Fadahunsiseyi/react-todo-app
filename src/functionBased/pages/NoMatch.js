cf9703a0bbad8d28a90c2faf7dd8ae24ec00553a
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-10 15:27:20 +0100

    Updates

--- src/functionBased/pages/NoMatch.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const NoMatch = () => <div>No match for this page</div>;
-
-export default NoMatch;
--- src/functionBased/pages/SinglePage.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-
-const SinglePage = () => <div>Hello from single page</div>;
-export default SinglePage;
