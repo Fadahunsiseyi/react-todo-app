2fcbbe478ebf8c9b5bedcebf360ae2a4260bdba9
Fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2022-08-10 13:13:50 +0100

    Fixing linters error

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -1,8 +1,6 @@
 import React from "react";
-import { useParams } from "react-router-dom";
 
 const SinglePage = () => {
-  let { id } = useParams();
   return <div>Hello from single page</div>;
 };
 export default SinglePage;
